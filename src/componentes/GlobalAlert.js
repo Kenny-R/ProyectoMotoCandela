d3607a9a8f0750195e369fe393c4a8156657a6ad
Ana <73360276+anna-ayn@users.noreply.github.com>
2023-07-03 16:40:56 -0400

    fixed alert

--- src/componentes/GlobalAlert.js
+++ src/componentes/GlobalAlert.js
@@ -1,20 +1,21 @@
 import { Alert, Fade } from "@mui/material";
-import { useGlobalAlert } from "../hooks/useGlobalAlert"
+import { useGlobalAlert } from "../hooks/useGlobalAlert";
 
 const GlobalAlert = () => {
-    const { alert } = useGlobalAlert();
+  const { alert } = useGlobalAlert();
 
-    return (Object.keys(alert).length === 0
-            ? <></> 
-            : 
-            <Fade in={alert.fade}>
-                <Alert 
-                    severity={alert.severity}
-                    style={{position:"absolute", top:0, left:0, zIndex:2000}}
-                    >
-                    {alert.text}
-                </Alert>
-            </Fade>)
+  return Object.keys(alert).length === 0 ? (
+    <></>
+  ) : (
+    <Fade in={alert.fade}>
+      <Alert
+        severity={alert.severity}
+        style={{ position: "fixed", zIndex: 2000 }}
+      >
+        {alert.text}
+      </Alert>
+    </Fade>
+  );
 };
 
 export default GlobalAlert;
